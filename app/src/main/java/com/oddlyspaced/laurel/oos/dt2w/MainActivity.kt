fc99617c1e375fca19f614fd3067f2f5beaee620
oddlyspaced <oddlyspaced1@gmail.com>
2020-03-12 16:26:15 +0530

    MainActivity: Check if shell command works

--- app/src/main/java/com/oddlyspaced/laurel/oos/dt2w/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/laurel/oos/dt2w/MainActivity.kt
@@ -2,11 +2,13 @@ package com.oddlyspaced.laurel.oos.dt2w
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.topjohnwu.superuser.Shell
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Shell.su("sendevent /dev/input/event2 0 1 5").submit();
     }
 }
